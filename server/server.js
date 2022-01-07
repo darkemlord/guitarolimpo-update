66eeff0b48d536ac94a05dadbbae7ee6b865528c
emanuel caro <darkemlord@gmail.com>
2022-01-07 16:44:58 +0900

    updating

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ Shopify.Context.initialize({
   API_KEY: process.env.SHOPIFY_API_KEY,
   API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
   SCOPES: process.env.SCOPES.split(","),
-  HOST_NAME: "https://guitarolimpo.herokuapp.com",
+  HOST_NAME: process.env.HOST.replace(/https:\/\/|\/$/g, ""),
   API_VERSION: ApiVersion.October20,
   IS_EMBEDDED_APP: true,
   // This should be replaced with your preferred storage strategy
