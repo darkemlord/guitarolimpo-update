9b166887ddf386b2f6a648c04eba275008c64c92
emanuel caro <darkemlord@gmail.com>
2022-01-07 17:30:25 +0900

    updating text

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import Products from "./components/Products";
 
 const Index = () => (
   <Page>
-    <Heading>My ema shop, dying with this! 🎉</Heading>
+    <Heading>
+      My ema shop, dying with this! but I'm going to solve it 🎉
+    </Heading>
     <Products />
   </Page>
 );
